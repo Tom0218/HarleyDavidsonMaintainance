7794d7797246ad1e7e347b41f6982b0eb7bb4bb4
HsiehYuTeng <mark29737148mark@gmail.com>
2024-06-17 13:34:50 +0800

    Build Header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
 </template>
+
+<style lang="scss" scoped>
+</style>
